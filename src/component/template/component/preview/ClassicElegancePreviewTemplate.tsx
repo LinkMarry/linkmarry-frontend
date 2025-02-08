69a14a4a29ce2ef4ea44b6ea22d269472bd9ffe7
hhhello <hhhello0507@dgsw.hs.kr>
2025-02-08 16:25:14 +0900

    fix: increaseFontSize

--- src/component/template/component/preview/ClassicElegancePreviewTemplate.tsx
+++ src/component/template/component/preview/ClassicElegancePreviewTemplate.tsx
@@ -1,13 +1,36 @@
 import React, {ComponentProps} from 'react';
 import PreviewTemplate from "@src/component/template/component/preview/PreviewTemplate";
+import {Column} from "@designsystem/component/FlexLayout";
+import {css} from "styled-components";
+import Text from "@designsystem/component/Text";
+import {format} from "date-fns";
+import {getDetails} from "@remote/value/WeddingSchedule";
+import CustomStyle from "@designsystem/component/CustomStyle";
+import {getBaseInfoByBrideMarkFirst} from "@remote/value/BaseInfo";
 
 function ClassicElegancePreviewTemplate(
     {
-        
+        baseInfo,
+        weddingSchedule,
+        weddingPlace,
     }: ComponentProps<typeof PreviewTemplate>
 ) {
+    const {first, second} = getBaseInfoByBrideMarkFirst(baseInfo);
+    const {date, isValidDate} = getDetails(weddingSchedule);
+
     return (
-        <div></div>
+        <Column $alignItems={'stretch'}>
+            <CustomStyle as={'img'} $customStyle={css`
+                
+            `}/>
+            <Column gap={8} $customStyle={css`
+                padding: 32px 8px;
+            `}>
+                {isValidDate && (
+                    <Text>{format(date, 'yyyy. MM. dd E HH:mm')}</Text>
+                )}
+            </Column>
+        </Column>
     );
 }
 
