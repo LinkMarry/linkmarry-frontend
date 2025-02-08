63397f40608fb3e17b91298d8e6e0566583cbd32
hhhello <hhhello0507@dgsw.hs.kr>
2025-02-08 14:36:33 +0900

    feat: 새 프리뷰 파일 추가

---
+++ src/component/template/component/preview/PreviewModernLoveTemplate.tsx
@@ -0,0 +1,14 @@
+import React, {ComponentProps} from 'react';
+import PreviewTemplate from "@src/component/template/component/preview/PreviewTemplate";
+
+function PreviewModernLoveTemplate(
+    {
+        
+    }: ComponentProps<typeof PreviewTemplate>
+) {
+    return (
+        <div></div>
+    );
+}
+
+export default PreviewModernLoveTemplate;
---
+++ src/component/template/component/preview/PreviewNaturalGardenTemplate.tsx
@@ -0,0 +1,14 @@
+import React, {ComponentProps} from 'react';
+import PreviewTemplate from "@src/component/template/component/preview/PreviewTemplate";
+
+function PreviewNaturalGardenTemplate(
+    {
+        
+    }: ComponentProps<typeof PreviewTemplate>
+) {
+    return (
+        <div></div>
+    );
+}
+
+export default PreviewNaturalGardenTemplate;
