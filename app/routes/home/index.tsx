b51f35825d7b7298b11031f026d89f5481467448
lkh0310kr <lkh0310kr@gmail.com>
2026-04-18 20:42:52 +0900

    가격 변경 8700 -> 12800, 100가지 스타일 -> 다양한

--- app/routes/home/index.tsx
+++ app/routes/home/index.tsx
@@ -682,7 +682,7 @@ function Section4({onViewSample}: Section4Props) {
                             line-height: 150%;
                         }
                     `}>
-                        100가지가 넘는 다양한 스타일, 나만의 청첩장을 무료로 제작
+                        다양한 스타일, 나만의 청첩장을 무료로 제작
                     </Text>
                 </View>
                 <View ui={cx(
@@ -1200,7 +1200,7 @@ function Section6({onNavigateNaverStore}: Section6Props) {
                                             line-height: 140%;
                                         }
                                     `}>
-                                        8,700원
+                                        12,800원
                                     </Text>
                                 </View>
                             </View>
